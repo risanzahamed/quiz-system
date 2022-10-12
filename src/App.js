cac251be6e45490ad5ff7985f92c5543ba0fb9d3
Risan Ahamed <risanzahamed76@gmail.com>
2022-10-12 21:42:50 +0600

    CLEAN

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './Routes/Routes'
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     errorElement: <ErrorPage />,
-//     element: <Root />,
-//     children: [
-//       { path: '/', element: <Home /> },
-      
-//       {
-//         path: 'books',
-//         element: <Books />,
-//         loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
-//       },
-
-
-//       { path: '/about', element: <About /> },
-//       { path: '/contact', element: <Contact /> }
-//     ],
-
-//   }
-
-// ])
 
 function App() {
   return (
